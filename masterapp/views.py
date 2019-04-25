f06c80eca9d411bec8a2a27f2bef72e58baf6dd8
zepk <zepktausao@gmail.com>
2019-04-25 11:30:08 -0400

    fixed wrong function name

--- masterapp/views.py
+++ masterapp/views.py
@@ -5,7 +5,6 @@ from .modules.funciones_bodega import *
 
 # Create your views here.
 
-
 def index(request):
     almacenes = obtener_almacenes()
     template = loader.get_template('masterapp/index.html')
