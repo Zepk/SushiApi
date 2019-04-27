4f8a6aa333d5e9e430820df976762d33cbf98f48
mealvarado <mealvarado@uc.cl>
2019-04-27 11:52:55 -0400

    se modificaron las funciones para que retornen todo la response del request

--- masterapp/modules/funciones_bodega.py
+++ masterapp/modules/funciones_bodega.py
@@ -3,6 +3,39 @@ from .hashing import *
 import requests
 import json
 
+
+def mover_productos_entre_almacenes(id_almacen, id_producto):
+    mensaje = "POST{}{}".format(id_producto, id_almacen)
+    aut = security_hash(mensaje, key)
+    url = 'https://integracion-2019-dev.herokuapp.com/bodega/moveStock'
+    headers = {'content-type': 'application/json', "Authorization": "INTEGRACION grupo{}:{}".format(grupo, aut)}
+    payload = {"productoId": id_producto, "almacenId": id_almacen}
+    r = requests.post(url, headers=headers, data=json.dumps(payload))
+    if r.status_code == 200:
+        return r
+        #si queremos retornar la lista de diccionarios
+        #lista = json.loads(r.text)
+        #return lista
+    else:
+        return r
+
+
+def mover_productos_entre_bodegas(id_producto, id_almacen):
+    mensaje = "POST{}{}".format(id_producto, id_almacen)
+    aut = security_hash(mensaje, key)
+    url = 'https://integracion-2019-dev.herokuapp.com/bodega/moveStockBodega'
+    headers = {'content-type': 'application/json', "Authorization": "INTEGRACION grupo{}:{}".format(grupo, aut)}
+    payload = {"productoId": id_producto, "almacenId": id_almacen, "precio": 10}
+    r = requests.post(url, headers=headers, data=json.dumps(payload))
+    if r.status_code == 200:
+        return r
+        #si queremos retornar la lista de diccionarios
+        #lista = json.loads(r.text)
+        #return lista
+    else:
+        return r
+
+
 # Consultar por almacenes del grupo
 def obtener_almacenes():
     mensaje = "GET"
@@ -11,12 +44,12 @@ def obtener_almacenes():
     headers = {'content-type': 'application/json', "Authorization" : "INTEGRACION grupo{}:{}".format(grupo, aut)}
     r = requests.get(url, headers=headers)
     if r.status_code == 200:
-        return r.text
+        return r
         #si queremos retornar la lista de diccionarios
         #lista = json.loads(r.text)
         #return lista
     else:
-        return r.status_code
+        return r
 
 
 def obtener_productos_en_almacen(almacenId, sku):
@@ -26,9 +59,9 @@ def obtener_productos_en_almacen(almacenId, sku):
     headers = {'content-type': 'application/json', "Authorization": "INTEGRACION grupo{}:{}".format(grupo, aut)}
     r = requests.get(url, headers=headers)
     if r.status_code == 200:
-        return r.text
+        return r
     else:
-        return r.status_code
+        return r
 
 
 # Consultar por skus con stock en un almacen, pide la id del almacen
@@ -39,29 +72,12 @@ def obtener_skus_con_stock(id_almacen):
     headers = {'content-type': 'application/json', "Authorization" : "INTEGRACION grupo{}:{}".format(grupo, aut)}
     r = requests.get(url, headers=headers)
     if r.status_code == 200:
-        return r.text
+        return r
         #si queremos retornar la lista de diccionarios
         #lista = json.loads(r.text)
         #return lista
     else:
-        return r.status_code
-
-
-def mover_productos_entre_almacenes(id_producto, id_almacen):
-    mensaje = "POST{}{}".format(id_producto, id_almacen)
-    aut = security_hash(mensaje, key)
-    url = 'https://integracion-2019-dev.herokuapp.com/bodega/moveStock'
-    headers = {'content-type': 'application/json', "Authorization": "INTEGRACION grupo{}:{}".format(grupo, aut)}
-    payload = {"productoId": id_producto, "almacenId": id_almacen}
-    r = requests.post(url, headers=headers, data=json.dumps(payload))
-    if r.status_code == 200:
-        return r.text
-        #si queremos retornar la lista de diccionarios
-        #lista = json.loads(r.text)
-        #return lista
-    else:
-        print(r.text)
-        return r.status_code
+        return r
 
 
 def fabricar_producto(sku, cantidad):
@@ -73,9 +89,9 @@ def fabricar_producto(sku, cantidad):
     r = requests.put(url, headers=headers, data=json.dumps(payload))
     print(r.text)
     if r.status_code == 200:
-        return r.text
+        return r
     else:
-        return r.status_code
+        return r
 
 
 def fabrica_obtener_cuenta():
@@ -87,9 +103,9 @@ def fabrica_obtener_cuenta():
     print(r.text)
     print(r.status_code)
     if r.status_code == 200:
-        return r.text
+        return r
     else:
-        return r.status_code
+        return r
 
 
 def eliminar_hook():
@@ -98,12 +114,10 @@ def eliminar_hook():
     url = 'https://integracion-2019-dev.herokuapp.com/bodega/hook'
     headers = {'content-type': 'application/json', "Authorization" : "INTEGRACION grupo{}:{}".format(grupo, aut)}
     r = requests.delete(url, headers=headers)
-    print(r.text)
-    print(r.status_code)
     if r.status_code == 200:
-        return r.text
+        return r
     else:
-        return r.status_code
+        return r
 
 
 def obtener_hook():
@@ -112,12 +126,10 @@ def obtener_hook():
     url = 'https://integracion-2019-dev.herokuapp.com/bodega/hook'
     headers = {'content-type': 'application/json', "Authorization" : "INTEGRACION grupo{}:{}".format(grupo, aut)}
     r = requests.get(url, headers=headers)
-    print(r.text)
-    print(r.status_code)
     if r.status_code == 200:
-        return r.text
+        return r
     else:
-        return r.status_code
+        return r
 
 
 def setear_hook(url):
@@ -127,9 +139,7 @@ def setear_hook(url):
     headers = {'content-type': 'application/json', "Authorization" : "INTEGRACION grupo{}:{}".format(grupo, aut)}
     payload = {"url": url}
     r = requests.put(url, headers=headers, data=json.dumps(payload))
-    print(r.text)
-    print(r.status_code)
     if r.status_code == 200:
-        return r.text
+        return r
     else:
-        return r.status_code
+        return r
