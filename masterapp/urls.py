5d5bcc99f77c006dff9ec25d5785f16ddf17050d
zepk <zepktausao@gmail.com>
2019-04-28 14:44:28 -0400

    docker setup

---
+++ masterapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('skus_stock/', views.skus_stock, name='skus_stock'),
+    path('productos_en_almacen/', views.productos_en_almacen, name='productos_en_almacen'),
+    path('fabricar_sin_pagar/', views.fabricar_sin_pagar, name='frabricar_sin_pagar'),
+    path('obtener_cuenta/', views.obtener_cuenta, name='obtener_cuenta'),
+    path('obtener_eliminar_hook/', views.obtener_eliminar_hook, name='obtener_eliminar_hook'),
+    path('obtener_obtener_hook/', views.obtener_obtener_hook, name='obtener_obtener_hook'),
+    path('obtener_setear_hook/', views.obtener_setear_hook, name='obtener_setear_hook'),
+]
--- masterapp/views.py
+++ masterapp/views.py
@@ -1,3 +1,80 @@
 from django.shortcuts import render
 
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .modules.funciones_bodega import *
+
 # Create your views here.
+
+
+def index(request):
+    almacenes = obtener_almacenes()
+    template = loader.get_template('masterapp/index.html')
+    context = {
+        'almacenes': almacenes,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def productos_en_almacen(request):
+    productos = obtener_productos_en_almacen(request.GET.get('almacenId', None), request.GET.get('sku', None))
+    template = loader.get_template('masterapp/productos_en_almacen.html')
+    context = {
+        'productos': productos,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def skus_stock(request):
+    skus = obtener_skus_con_stock(request.GET.get('almacenId', None))
+    template = loader.get_template('masterapp/sku_stock.html')
+    context = {
+        'skus': skus,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def fabricar_sin_pagar(request):
+    producto = fabricar_producto(request.GET.get('sku', None), request.GET.get('cantidad', None))
+    template = loader.get_template('masterapp/fabricar_sin_pagar.html')
+    context = {
+        'producto': producto,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def obtener_cuenta(request):
+    cuenta = fabrica_obtener_cuenta()
+    template = loader.get_template('masterapp/obtener_cuenta.html')
+    context = {
+        'cuenta': cuenta,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def obtener_eliminar_hook(request):
+    hook = eliminar_hook()
+    template = loader.get_template('masterapp/hook.html')
+    context = {
+        'hook': hook,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def obtener_obtener_hook(request):
+    hook = obtener_hook()
+    template = loader.get_template('masterapp/hook.html')
+    context = {
+        'hook': hook,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def obtener_setear_hook(request):
+    hook = setear_hook(request.GET.get('url', None))
+    template = loader.get_template('masterapp/hook.html')
+    context = {
+        'hook': hook,
+    }
+    return HttpResponse(template.render(context, request))
