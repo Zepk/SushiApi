4630d364834198a749f8916ff01eabb3cb356c4f
zepk <zepktausao@gmail.com>
2019-04-28 14:27:32 -0400

    switching to docker

--- masterapp/modules/constantes.py
+++
@@ -1,8 +0,0 @@
-key = 'l.akT6fMzi5gG'
-grupo = 6
-recepcion = "5cbd3ce444f67600049431d1"
-despacho = '5cbd3ce444f67600049431d2'
-almacen_general1 = '5cbd3ce444f67600049431d3'
-almacen_general2 = '5cbd3ce444f67600049431d4'
-pulmon = '5cbd3ce444f67600049431d5'
-cocina = '5cbd3ce444f67600049431d6'
