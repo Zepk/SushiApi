dd7a71e0d0851e0d6d4e4ad2ca659985c40032f3
zepk <paacuna@uc.cl>
2019-04-29 18:01:53 -0400

    fixed vaciar_recepcion_y_pulmon

--- masterapp/tasks.py
+++ masterapp/tasks.py
@@ -6,17 +6,16 @@ from .modules.funciones_bodega import *
 def vaciar_recepcion_y_pulmon():
     almacenes = json.loads(obtener_almacenes())
     for almacen in almacenes:
-        if almacen['_id'] == pulmon and almacen['usedSpace'] != 0:
-            for almacen2  in almacenes:
+        if (almacen['_id'] == pulmon or almacen['_id'] == recepcion) and almacen['usedSpace'] != 0:
+            for almacen2 in almacenes:
                 if almacen2['_id'] == almacen_general1 or almacen2['_id'] == almacen_general2:
                     if almacen2['totalSpace'] > almacen2['usedSpace']:
-                        skus = json.loads(obtener_skus_con_stock(pulmon))
+                        skus = json.loads(obtener_skus_con_stock(almacen['_id']))
                         for sku in skus:
                             sku = sku['_id']
-                            productos = json.loads(obtener_productos_en_almacen(pulmon, sku))
+                            productos = json.loads(obtener_productos_en_almacen(almacen['_id'], sku))
                             for producto in productos:
                                 mover_productos_entre_almacenes(producto['_id'], almacen2['_id'])
-
 
 @shared_task
 def probando_celery():
