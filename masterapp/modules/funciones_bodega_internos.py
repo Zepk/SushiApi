13b0744010891840e16b8a3d458b1c28a3a31ec8
zepk <paacuna@uc.cl>
2019-05-03 01:25:41 -0400

    added a print

--- masterapp/modules/funciones_bodega_internos.py
+++ masterapp/modules/funciones_bodega_internos.py
@@ -52,6 +52,7 @@ def despachar_un_producto(productoId, almacenId, precio):
     print(productoId)
     r = mover_productos_entre_bodegas(productoId, almacenId)
     if r.status_code == 200:
+        print("Producto enviado")
         return True
     else:
         return False
