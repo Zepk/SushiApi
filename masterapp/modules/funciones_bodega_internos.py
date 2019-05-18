602d21c7c39586b157686f7b58ebde2c482b33dd
zepk <paacuna@uc.cl>
2019-05-18 19:11:27 -0400

    added try excepts

--- masterapp/modules/funciones_bodega_internos.py
+++ masterapp/modules/funciones_bodega_internos.py
@@ -8,17 +8,20 @@ from .funciones_bodega import *
 def stock_disponible():
     almacenes = obtener_almacenes()
     inventario = {}
-    for almacen in json.loads(almacenes):
-        if int(almacen["usedSpace"]) > 0:
-            for skus in json.loads(obtener_skus_con_stock(almacen["_id"])):
-                if skus["_id"] in inventario.keys():
-                    inventario[skus["_id"]] += skus["total"]
-                else:
-                    inventario[skus["_id"]] = skus["total"]
-    respuesta = []
-    for k, v in inventario.items():
-        respuesta.append({"sku": k, "total": v})
-    return respuesta
+    try:
+        for almacen in json.loads(almacenes):
+            if int(almacen["usedSpace"]) > 0:
+                for skus in json.loads(obtener_skus_con_stock(almacen["_id"])):
+                    if skus["_id"] in inventario.keys():
+                        inventario[skus["_id"]] += skus["total"]
+                    else:
+                        inventario[skus["_id"]] = skus["total"]
+        respuesta = []
+        for k, v in inventario.items():
+            respuesta.append({"sku": k, "total": v})
+        return respuesta
+    except TypeError:
+        pass
 
 # Retorna un diccionario que contiene todas las claves sku: cantidad_disponible
 def contar_productos():
@@ -39,14 +42,16 @@ def stock_disponible_sku(sku, cantidad):
 def obtener_almacenes_con_sku(sku):
     almacenes = obtener_almacenes()
     inventario = {}
-    for almacen in json.loads(almacenes):
-        if int(almacen["usedSpace"]) > 0:
-            for skus in json.loads(obtener_skus_con_stock(almacen["_id"])):
-                if skus["_id"] == sku:
-                    inventario[almacen["_id"]] = {"sku": skus["_id"], "total": skus["total"], "despacho": almacen["despacho"]}
-                    break
-    return inventario
-
+    try:
+        for almacen in json.loads(almacenes):
+            if int(almacen["usedSpace"]) > 0:
+                for skus in json.loads(obtener_skus_con_stock(almacen["_id"])):
+                    if skus["_id"] == sku:
+                        inventario[almacen["_id"]] = {"sku": skus["_id"], "total": skus["total"], "despacho": almacen["despacho"]}
+                        break
+        return inventario
+    except TypeError:
+        pass
 
 def despachar_un_producto(productoId, almacenId, precio):
     r = mover_productos_entre_bodegas(productoId, almacenId)
